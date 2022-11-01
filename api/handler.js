4e9c519433621faac36bd94239dde8f50b30e87e
diegohpezet <74683374+diegohpezet@users.noreply.github.com>
2022-10-31 21:05:05 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -1,7 +1,8 @@
 export default function handler(request, response) {
     response.status(200).json({
-      id: request.id,
-      damage_relations: request.damage_relations,
+      body: request.body,
+      query: request.query,
+      cookies: request.cookies,
     });
   }
   
