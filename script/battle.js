e08c942d9486f4a78354eec100f516251ebaf907
diegohpezet <74683374+diegohpezet@users.noreply.github.com>
2022-11-07 19:15:29 -0300

    Update battle.js

--- script/battle.js
+++ script/battle.js
@@ -1,5 +1,5 @@
 function battle(type1, type2) {
-    fetch(`/api/duel?type1=${type1}&type2=${type2}`)
+    fetch(`https://pokemon-api-game.vercel.app/api/duel?type1=${type1}&type2=${type2}`)
     .then((response) => response.json())
     .then((data) => {
         if (data.response) {
