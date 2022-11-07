f9e3c27a6aaf85b231dc184414be01dc43a72c89
diegohpezet <74683374+diegohpezet@users.noreply.github.com>
2022-11-07 20:23:23 -0300

    Update duel.js

--- api/duel.js
+++ api/duel.js
@@ -1,22 +1,11 @@
 export default function handler(request,response){
     console.log("Sending data...")
-    if (type1 = "fire") {
-        response.status(200).json({
-            /*
-            body: request.body,
-            query: request.query,
-            cookies: request.cookies,
-            */
-            result: true
-        });
-    } else {
-        response.status(200).json({
-            /*
-            body: request.body,
-            query: request.query,
-            cookies: request.cookies,
-            */
-            result: false
-        });
-    }
+    response.status(200).json({
+        /*
+        body: request.body,
+        query: request.query,
+        cookies: request.cookies,
+        */
+        result: true
+    });
 }
