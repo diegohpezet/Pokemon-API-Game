39835b832c5f345b41742b2f7d60f0e987da00f0
diegohpezet <74683374+diegohpezet@users.noreply.github.com>
2022-11-07 23:20:34 -0300

    Update duel.js

--- api/duel.js
+++ api/duel.js
@@ -1,16 +1,7 @@
-import fetch from 'node-fetch';
-
 export default function handler(request,response){
     let attackingType = request.query.type1
     let defendingType = request.query.type2
     
-    /* Fetch attacking type */
-    let info = fetch("https://pokeapi.co/api/v2/type/" + attackingType)
-    let strong_against = info.json()
-
-    console.log(strong_against)
-
-
     /* Evaluate types */
 
     let result = {
