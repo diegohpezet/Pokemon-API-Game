34bc3983e0c625aead796a898bc9f21a92a4713a
diegohpezet <74683374+diegohpezet@users.noreply.github.com>
2022-11-07 23:01:16 -0300

    Update battle.js

--- script/battle.js
+++ script/battle.js
@@ -1,4 +1,6 @@
 const resultContainer = document.getElementById("result")
+const pointCounter = document.getElementById("points")
+let points = 0
 
 function battle(type1, type2) {
     fetch(`https://pokemon-api-game.vercel.app/api/duel?type1=${type1}&type2=${type2}`)
@@ -7,6 +9,8 @@ function battle(type1, type2) {
         console.log(data.result.isCorrect)
         if (data.result.isCorrect) {
             resultContainer.innerText = "Win"
+            points++
+            resultContainer.innerText = "Points " + points
         } else {
             resultContainer.innerText = "Lose"
         }
