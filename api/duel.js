3890bb792a7c7ef342b685a99efe559955021bec
diegohpezet <74683374+diegohpezet@users.noreply.github.com>
2022-11-09 00:16:10 -0300

    Update duel.js

--- api/duel.js
+++ api/duel.js
@@ -1,35 +1,38 @@
 import fetch from "node-fetch"
 
-export default function handler(request,response){
-    let attackingType = request.query.type1
-    let defendingType = request.query.type2
+export default async function handler(request,response){
+    try {
+        let attackingType = request.query.type1
+        let defendingType = request.query.type2
     
-    url = `https://pokeapi.co/api/v2/type/${attackingType}`
-
-   
-    const pokeApiRes = await fetch(url)
-    console.log(pokeApiRes)
+        url = `https://pokeapi.co/api/v2/type/${attackingType}`
     
-    /* Evaluate types */
-
-    let result = {
-        isCorrect: false,
-        expectedType: false
-    }
-
-    if (attackingType === "fire") {
-        result.isCorrect = true
-    } 
+        const pokeApiRes = await fetch(url)
+        console.log(pokeApiRes)
+           
+        /* Evaluate types */
     
-    if(!result.isCorrect){
-        result.expectedType = attackingType
-    }
-
-    console.log(result)
-
-    /* Generate response */
-
-    response.status(200).json({
-        result
-    });
+        let result = {
+            isCorrect: false,
+            expectedType: false
+        }
+    
+        if (attackingType === "fire") {
+            result.isCorrect = true
+        } 
+        
+        if(!result.isCorrect){
+            result.expectedType = defendingType
+        }
+    
+        console.log(result)
+    
+        /* Generate response */
+    
+        response.status(200).json({
+            result
+        });
+      } catch (err) {
+         console.log(err)
+      }
 }
