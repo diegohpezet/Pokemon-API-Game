11fd72d7793b9bfbe4125b13dc494e4534b89449
Brisa-dlC <101837577+Brisa-dlC@users.noreply.github.com>
2022-11-08 23:35:22 -0300

    Add files via upload

--- script/choose.js
+++ script/choose.js
@@ -23,26 +23,36 @@ types = [
     "dark",
     "fairy"
 ]
+/*
+async function chooseTypes() {
+    const getRandomIndex = (arr) => Math.floor(Math.random()*types.length)
+
+    const type_1 = types[getRandomIndex(types)]
+    let type_2 = types[getRandomIndex(types)]
+    
+    while(type_2 === type_1)
+            type_2 = types[getRandomIndex(types)]
+    
+    return [type_1, type_2]
+}*/
 
 async function chooseTypes() {
     let type_1 = types[Math.floor(Math.random()*types.length)];
     let type_2 = types[Math.floor(Math.random()*types.length)];
-
-    return [type_1, type_2]
+    while (type_1===type_2){
+           type_2 = types[Math.floor(Math.random()*types.length)];
+        }
+        return [type_1, type_2]
 }
 
 chooseTypes().then(([type_1, type_2]) => {
     //Sets colors of containers
     container_1.style.backgroundColor = "var(--"+type_1+")"
     container_2.style.backgroundColor = "var(--"+type_2+")"
-    
     //Sets images of containers
     img_1.src=`./img/types/${type_1}.png`
     img_2.src=`./img/types/${type_2}.png`
-
-    //Generates an onclick event
-    container_1.setAttribute("onclick",`battle('${type_1}','${type_2}')`)
-    container_2.setAttribute("onclick",`battle('${type_2}','${type_1}')`)
+    
 })
 
 chooseTypes()
