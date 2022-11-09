7ae88e4fa9ceb5dbd7e8e8e84d2ca75b0fbe50d4
diegohpezet <74683374+diegohpezet@users.noreply.github.com>
2022-11-09 00:33:32 -0300

    Update duel.js

--- api/duel.js
+++ api/duel.js
@@ -1,42 +1,49 @@
-import fetch from "node-fetch"
+import fetch from "node-fetch";
 
-export default async function handler(request,response){
-    try {
-        let attackingType = request.query.type1
-        let defendingType = request.query.type2
-    
-        /* Fetch data from chosen type */
-        let url = `https://pokeapi.co/api/v2/type/${attackingType}`
-    
-        let pokeApiRes = await fetch(url)
-        let data = await pokeApiRes.json()
+export default async function handler(request, response) {
+  try {
+    let attackingType = request.query.type1;
+    let defendingType = request.query.type2;
 
-        let hits_hard = data.damage_relations.double_damage_to
+    /* Fetch data from chosen type */
+    let url = `https://pokeapi.co/api/v2/type/${attackingType}`;
 
-        /* Evaluate types */
-    
-        let result = {
-            isCorrect: false,
-            expectedType: false
-        }
-    
-        if (attackingType === "fire") {
-            result.isCorrect = true
-        } 
-        
-        if(!result.isCorrect){
-            result.expectedType = defendingType
-        }
-    
-        console.log(result)
-    
-        /* Generate response */
-    
-        response.status(200).json({
-            result, hits_hard
-        });
-      } catch (err) {
-         console.error(err)
-      }
+    let pokeApiRes = await fetch(url);
+    let data = await pokeApiRes.json();
+
+    /* Evaluates strengths */
+
+    let super_effective_against = data.damage_relations.double_damage_to;
+    let hits_hard = [];
+
+    super_effective_against.forEach((element) => {
+      hits_hard.push(element.name);
+    });
+
+    /* Evaluate types */
+
+    let result = {
+      isCorrect: false,
+      expectedType: false,
+    };
+
+    if (attackingType === "fire") {
+      result.isCorrect = true;
+    }
+
+    if (!result.isCorrect) {
+      result.expectedType = defendingType;
+    }
+
+    console.log(result);
+
+    /* Generate response */
+
+    response.status(200).json({
+      result,
+      hits_hard,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
-
