8a1ff63fd5c35ba24e1cf4ee0d18c484e61b10a6
diegohpezet <74683374+diegohpezet@users.noreply.github.com>
2022-11-08 22:50:11 -0300

    Update duel.js

--- api/duel.js
+++ api/duel.js
@@ -2,8 +2,9 @@ export default function handler(request,response){
     let attackingType = request.query.type1
     let defendingType = request.query.type2
     
-    let fetchedType = fetch(`https://pokeapi.co/api/v2/type/${attackingType}`)
-    let data = fetchedType.json()
+    fetch(`https://pokeapi.co/api/v2/type/${attackingType}`)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
     console.log(data)
     
